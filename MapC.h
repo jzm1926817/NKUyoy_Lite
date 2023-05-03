8fc1cfb680579f8b8c8959b2f7b15c91a45e30b2
Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>
2023-05-03 12:15:58 +0000

    删除文件 MapC.h

--- MapC.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<cmath>
-#include<iostream>
-
-extern int ma[20][20],r[20][20],q[7][400];
-void MapGeneration(int tal);
