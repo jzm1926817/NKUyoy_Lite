409bb9da2eb6ec3c0baa1d43bb6dc822102f3923
Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>
2023-05-05 16:55:28 +0000

    添加音乐播放器（半成品），修复bug 添加了音乐播放器并修复了部分bug，但是音乐播放器目前存在只能播放一遍而不能循环播放的bug
    Signed-off-by: Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>

---
+++ 第十一次：音乐播放器/common.cpp
@@ -0,0 +1,23 @@
+#include "common.h"
+
+COLORREF _Colors[10] = {
+	RGB(128, 128, 128),
+	RGB(255, 0, 0),
+	RGB(0, 255, 0),
+	RGB(0, 0, 255),
+	RGB(255, 255, 0),
+	RGB(255, 0, 255),
+	RGB(0, 255, 255),
+	RGB(128, 0, 0),
+	RGB(0, 128, 0),
+	RGB(0, 0, 128),
+};
+
+void _InitWin()
+{
+	initgraph(WIN_W, WIN_H, EX_SHOWCONSOLE);
+	setbkcolor(COLOR3);
+	cleardevice();
+	setbkmode(0);
+}
+
