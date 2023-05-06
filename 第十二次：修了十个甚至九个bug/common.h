a946621f6753a30ca707a9edaa332682959440c5
Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>
2023-05-06 13:31:24 +0000

    修了十个甚至九个Bug 修复了： 1.音乐播放器完工（下详细介绍） 2.某些地方军队无法操作的bug 3.导出编辑器存档出现的一些bug 4.数钱的一些bug 5.音乐播放器不能随机播放的bug 6.音乐播放器随机播放出同一首曲子的bug 7.音乐播放器播不出歌的bug 8.音乐播放器随机播放触发不了的bug 9.音乐播放器显示错误的bug 10.音乐播放器的其他一些bug 算上第一个是10个，去掉第一个是9个，所以十个甚至9个bug（赞赏）
    Signed-off-by: Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>

---
+++ 第十二次：修了十个甚至九个bug/common.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<easyx.h>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<graphics.h>
+#include<mmsystem.h>
+#include<windows.h>
+//#include<mmsystem.h> 
+#pragma warning(disable:4996)
+
+#pragma comment (lib,"winmm.lib");
+
+
+
+#define IMG_W 40
+#define IMG_H 40
+
+#define WIN_W 1024
+#define WIN_H 800
+
+#define COLOR1 RGB(139,171,255)
+#define COLOR2 RGB(140,140,255)
+#define COLOR3 RGB(240,240,240)
+
+
+
+void _InitWin();
