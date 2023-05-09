3d0a195a6d2157ebfd4e80d678bdd975afe29231
Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>
2023-05-09 16:17:02 +0000

    特别神奇，一大堆我曾经修过的bug突然出现了，但是我没改过任何代码
    Signed-off-by: Zhenjie Zhang <12755332+Kyotsuki_Tankou@user.noreply.gitee.com>

---
+++ 第十六次/common.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<easyx.h>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<graphics.h>
+#include<mmsystem.h>
+#include<windows.h>
+//#include<mmsystem.h> 
+
+
+
+
+#define IMG_W 40
+#define IMG_H 40
+
+#define WIN_W 1024
+#define WIN_H 800
+
+#define COLOR1 RGB(139,171,255)
+#define COLOR2 RGB(140,140,255)
+#define COLOR3 RGB(240,240,240)
+
+
+
+void _InitWin();
